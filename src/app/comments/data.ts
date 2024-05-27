28420bf2784f5ba6491e960cd268adddf5e6c199
Ngoufack Edgard <ngoufackedgard1@gmail.com>
2024-05-27 13:42:02 +0100

    route handler, cookies, headers and middlewares

---
+++ src/app/comments/data.ts
@@ -0,0 +1,23 @@
+const data:{id:number,text:string,user:string}[]=[
+    {
+        id:1,
+        text:"Hello world",
+        user:"Me"
+    },
+    {
+        id:2,
+        text:"Bonjour quoi de neuf ?",
+        user:"Edgard"
+    },
+    {
+        id:3,
+        text:"Rien, je vais bien",
+        user:"Me"
+    },
+    {
+        id:4,
+        text:"Comment se porte le travail",
+        user:"Edgard"
+    },
+]
+export default data
